de9be0cebb6d593de10aed696973c8571a41d1e7
Nicolás Rojas <64169554+onicalls@users.noreply.github.com>
2022-04-27 15:23:51 -0400

    Update tdaCards_20058348_RojasGonzalez.pl
    todo hasta missingcards

--- tdaCards_20058348_RojasGonzalez.pl
+++ tdaCards_20058348_RojasGonzalez.pl
@@ -144,6 +144,7 @@ pertenece(Elemento, [_|Resto], Cont):-
 
 %Ejemplo: validador([1,2,3],[1,4,5],0).
 %Ejemplo: trace, (validador([1,2,3],[1,2,5],0,A)).
+validador(_,[],0,1).
 validador([],_,Cont,Cont).
 validador(C1,C2,I,Cont):-
     primerElementoDeUnaLista(C1,E1),
@@ -152,19 +153,6 @@ validador(C1,C2,I,Cont):-
 	restoDeUnaLista(C1,C11),
 	validador(C11,C2,Cont2,Cont),!.
 
-validador2(A):-
-    A<2,
-    A>0.
-
-compararCartas([]).
-compararCartas(CS):-
-    primerElementoDeUnaLista(CS,Card1),
-    restoDeUnaLista(CS,Resto),
-    primerElementoDeUnaLista(Resto,Card2),
-	validador(Card1,Card2,A),
-    validador2(A),
-    restoDeUnaLista(CS,Cards),
-    compararCartas(Cards),!.
 
 %cardsSet([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], 3, MaxC,Seed,CS).
 %cardsSet([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], 3,5,1239,CS).
@@ -180,4 +168,25 @@ cardsSet(Elements,NumE,MaxC,Seed,CS):-
     verificarMaxC(MaxC,NumCartas,MaxC),
     limitadorDeCartas(F,MaxC,MaxC,[],F2),
     randomList(Seed,F2,[],CS),!.
-    
+
+%Ejemplo: cardsSetIsDobble([[1,2,3],[1,4,5], [1,6,7]]).
+cardsSetIsDobble([]).
+cardsSetIsDobble(CS):-
+    primerElementoDeUnaLista(CS,Card1),
+    restoDeUnaLista(CS,Resto),
+    primerElementoDeUnaLista(Resto,Card2),
+	validador(Card1,Card2,0,A),
+    A<2,
+    A>0,
+    restoDeUnaLista(CS,Cards),
+    cardsSetIsDobble(Cards),!.
+
+%cardsSetNthCard(CS,3,CS2).
+cardsSetNthCard(CS,N,CS2):-
+    obtenerElementoEnPosicion(CS,N,CS2),!.
+
+%cardsSetFindTotalCards(
+cardsSetFindTotalCards(C2,TC):-
+    length(C2,N),
+    N2 is N-1,
+    TC is N2*N2 + N2 + 1.
